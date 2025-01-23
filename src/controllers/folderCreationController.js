5eb43b39cb76306a7d5adfba1b83d986eab15496
MarkoMTP <marko.mtp8@gmail.com>
2025-01-23 22:45:39 +0100

    edited the folderCreationController and registrationController, so they dont try to locally create folders, just in the db

--- src/controllers/folderCreationController.js
+++ src/controllers/folderCreationController.js
@@ -6,15 +6,9 @@ const folderCreationController = async (req, res) => {
   const { folderName } = req.body;
   const user = req.user;
   const firstFolder1 = await firstFolder(user.id);
-  const UPLOADS_DIR = path.join(__dirname, "../uploads");
-
-  const userDirPath = path.join(UPLOADS_DIR, user.email, folderName);
 
   try {
-    if (!fs.existsSync(userDirPath)) {
-      fs.mkdirSync(userDirPath);
-      await addFolderToDb(folderName, user.id, firstFolder1.id);
-    }
+    await addFolderToDb(folderName, user.id, firstFolder1.id);
 
     res.redirect("/userPage");
   } catch (err) {
